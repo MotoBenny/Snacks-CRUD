8df83fa2b906fe152a1617bc5889527f90500925
Benjamin <benj.james.carter@gmail.com>
2022-05-25 13:24:36 -0700

    all tests passing

--- snacks/urls.py
+++ snacks/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', SnackListView.as_view(), name='snack_list'),
     path('<int:pk>/', SnackDetailView.as_view(), name='snack_detail'),
     path('create/', SnackCreateView.as_view(), name='snack_create'),
-    path('<int:pk>/update', SnackUpdateView.as_view(), name='update_snack'),
-    path('<int:pk>/delete', SnackDeleteView.as_view(), name='delete_snack')
+    path('<int:pk>/update', SnackUpdateView.as_view(), name='snack_update'),
+    path('<int:pk>/delete', SnackDeleteView.as_view(), name='snack_delete')
 ]
